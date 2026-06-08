ae45843bbf095b053c26a3cc257a5b5a7d03bb3c
ecomerford14102002 <ecomerford@deloitte.ie>
2026-06-08 12:27:26 +0100

    Update API Gateway URL for production environment HTTPS

--- WC_Prediction_Game-main/public/src/js/api.js
+++ WC_Prediction_Game-main/public/src/js/api.js
@@ -7,7 +7,7 @@ const API_BASE_URL = (() => {
         return 'http://localhost:5000';
     } else {
         // Use your API Gateway URL for production
-        return 'https://2cbqd4lkzf.execute-api.eu-west-1.amazonaws.com/prod';
+        return 'https://fvw5hp0zo5.execute-api.eu-west-1.amazonaws.com/prod';
     }
 })();
 
